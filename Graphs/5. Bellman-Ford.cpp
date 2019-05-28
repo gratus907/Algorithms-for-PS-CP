71520a6e1a5e26c2c3e3de81f56ba7f8e0d76b79
Wonseok Shin <gratus907@naver.com>
2019-05-28 23:48:24 +0900

    Bellman Ford Algorithm

---
+++ Graphs/5. Bellman-Ford.cpp
@@ -0,0 +1,38 @@
+const int INF = 987654321;
+struct Edge
+{
+    int u, v, w;
+};
+
+vector <Edge> edgelist;
+int V, E;
+int dist[502];
+
+bool relax_all_edge()
+{
+    bool flag = false;
+    for (auto it:edgelist)
+    {
+        int u = it.u, v = it.v, w = it.w;
+        if (dist[v] > dist[u] + w && (dist[u]!=INF))
+        {
+            flag = true;
+            dist[v] = dist[u]+w;
+        }
+    }
+    return flag;
+}
+
+int bellman_ford()
+{
+    fill(dist,dist+502,INF);
+    dist[1] = 0;
+    for (int i = 0; i<V-1; i++)
+    {
+        relax_all_edge();
+    }
+    if (relax_all_edge())
+        return -1;
+    else
+        return 0;
+}
