c6218c0aaba24e489d3c0a89b0703281af5615fd
Wonseok Shin <gratus907@naver.com>
2019-07-27 02:41:31 +0900

    FInalized Before UCPC 2019

--- code/Mathematics/Miller_Rabin_Test.cpp
+++ code/Mathematics/Miller_Rabin_Test.cpp
@@ -18,7 +18,8 @@ bool Miller_Rabin(ll n)
 {
     if (n<2)
         return false;
-    if (n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13 || n == 17)
+    if (n == 2 || n == 3 || n == 5 || n == 7 ||
+     n == 11 || n == 13 || n == 17)
         return true;
     if (n%2 == 0 || n%3 == 0 || n%5 == 0)
         return false;
