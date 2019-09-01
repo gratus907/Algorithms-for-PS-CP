629e56e820259b0d15d3785b6753340ea1096482
Wonseok Shin <gratus907@naver.com>
2019-09-02 00:02:08 +0900

    .

--- code/Header.cpp
+++ code/Header.cpp
@@ -2,6 +2,9 @@
 #pragma GCC optimize("O3")
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx,avx2,fma")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+#pragma GCC target("popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
 #define ll long long
 #define eps 1e-7
 #define all(x) ((x).begin()),((x).end())
